bef994094db9116c44182e02b8641e6130d55a3a
Johnny Hyytiäinen <Johnny.Hyytiainen@gmail.com>
2026-02-22 19:08:45 +0100

    feat(nested): added nested schema in schemas.py for category and source for endpoints

--- src/schemas.py
+++ src/schemas.py
@@ -5,12 +5,38 @@ from pydantic import BaseModel, ConfigDict
 from typing import Optional
 from src.models import DifficultyLevel
 
+# --- 1: Sub-schemas (relationer) ---
+
+class CategoryResponse(BaseModel): # Schema för att få fram Category
+    id: int
+    name: str
+
+    # Min "bro" för Pydantic till SQLAlchemy. Säger åt Pydantic att läsa direkt ifrån SQLAlchemy object
+    model_config = ConfigDict(from_attributes=True)
+
+# TODO: FYLL I SOURCES MED LÄNKAR FÖR VARJE GLOSA!!!
+class SourceResponse(BaseModel):
+    id: int
+    name: str
+    url: Optional[str] = None # Bra att ha med URL om jag fyller i mina sources med links 
+
+    # Min "bro" för Pydantic till SQLAlchemy. Säger åt Pydantic att läsa direkt ifrån SQLAlchemy object
+    model_config = ConfigDict(from_attributes=True)
+
+
+# --- 2: Main Schema ---
+
 # "Svars kontraktet", det mitt API skickar TILL användaren(Vad user ser)
 class TermResponse(BaseModel):
     id: int
     slug: str
     term: str
+    definition: str # <-- NY: För att få ut definitionen av glosan.
     difficulty: Optional[DifficultyLevel] = None
+
+    # THE NESTED MAGIC: Här säger jag att varje Term har en lista av Kategorier och Källor
+    categories: list[CategoryResponse] = []
+    sources: list[SourceResponse] = []
 
     # Min "bro" för Pydantic till SQLAlchemy. Säger åt Pydantic att läsa direkt ifrån SQLAlchemy object
     model_config = ConfigDict(from_attributes=True)
