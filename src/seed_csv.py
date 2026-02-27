8290ea81be1299ef485410fcdce943bb10b65a5a
Johnny Hyytiäinen <Johnny.Hyytiainen@gmail.com>
2026-02-27 19:57:03 +0100

    feat(refactor): refactored seed_csv script with mapping variable to include edge cases in glossary for slug generation

--- src/seed_csv.py
+++ src/seed_csv.py
@@ -7,21 +7,48 @@ from sqlalchemy import select
 from src.database import SessionLocal
 from src.models import Term, Category, Source, DifficultyLevel
 
+# Skapa 'translation map' för kända specialfall/edge cases
+# Här får jag mappa vad extremfallen ska heta i URL'en
+CUSTOM_SLUGS = {
+        "*args":"args",
+        "**kwargs": "kwargs",
+        "__repr__": "dunder-repr",
+        "__init__": "dunder-init",
+        "__str__": "dunder-str",
+        "__init__": "dunder-init",
+        "__iter__": "dunder-iter",
+        "c++": "cpp",
+        "cd ..": "cd-dotdot",
+        "c#": "c-sharp",
+        "docker exec -it": "docker-exec-it"
+    }
+
 def generate_slug(text: str) -> str:
     """
     Helper function to clean name and create a URL friendly slug.
-    Example: 'Primary Key (PK)' -> 'primary-key-pk'
+    Example: 'Primary Key (PK)' -> 'primary-key-pk'.
+    Respecting Python/Docker syntax.
     """
-    text = str(text).lower().strip()
-    # Var noga med att ha med viktiga tecken som kan behövas, t.ex 'cd ...'
-    text = text.replace('..', 'dotdot')
-    text = text.replace('c++', 'cpp')
-    # Byt ut allt som inte är bokstäver/siffror mot bindestreck
-    text = re.sub(r'[^a-z0-9]+', '-', text)
-    return text.strip('-')
+
+    # 1) Rensa markdown escapes direkt. (t.ex \*args blir *args)
+    clean_text = str(text).replace("\\", "").strip().lower()
+
+
+    # 2) Om termen finns i min mapping för CUSTOM SLUGS, returnera översättningen direkt
+    if clean_text in CUSTOM_SLUGS:
+        return CUSTOM_SLUGS[clean_text]
+    
+    # 4) Om det inte är ett specialfall/edge case kör standard slug generating
+    # Byt ut allt som inte är bokstäver/siffror mot bindestreck - 
+    slug = re.sub(r'[^a-z0-9]+', '-', clean_text)
+
+    # Rensa upp multibla bindestreck, --- blir - och bindestreck i ändarna
+    slug = re.sub(r'-+', '-', slug).strip('-')
+    
+    return slug
 
 def load_data_from_csv():
-    csv_file = "data/glossary_filled_clean.csv"
+    csv_file = "data/glossary_filled_unique_terms.csv"
 
     print(f"Loading: Reading {csv_file}...")
 
@@ -36,7 +63,8 @@ def load_data_from_csv():
     try:
         # loopa igenom varje rad i CSV
         for index, row in df.iterrows():
-            term_name = str(row['Term']).strip()
+            # Tvätta bort markdown escapes direkt
+            term_name = str(row['Term']).replace("\\", "").strip()
             definition = str(row['Definition']).strip()
             category_name = str(row['Category']).strip()
             source_name = str(row['Source']).strip()
